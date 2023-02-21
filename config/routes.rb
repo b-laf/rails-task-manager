1d4cb213b6c29dd8d64199df205e6eb641baed99
Bastien Lafont <lafont.bastien@gmail.com>
2023-02-21 11:49:16 +0100

    backup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get "tasks", to: "tasks#index"
+  get "tasks/:id", to: "tasks#show", as: "task"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
