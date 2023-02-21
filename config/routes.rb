992a3bf43ca9247407cbf92ebd6c3ae498ee5a66
Bastien Lafont <lafont.bastien@gmail.com>
2023-02-21 12:01:40 +0100

    end of create CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  get "tasks", to: "tasks#index"
-  get "tasks/:id", to: "tasks#show", as: "task"
+  get 'tasks', to: 'tasks#index'
+  get 'tasks/new', to: 'tasks#new'
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
+  post "tasks", to: "tasks#create"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
